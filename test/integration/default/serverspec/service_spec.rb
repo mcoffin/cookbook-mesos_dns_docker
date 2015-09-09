2e0fe5192e79d170b43e52f80a5916b8a94f5fd8
Matt Coffin <mcoffin13@gmail.com>
2015-09-09 12:32:25 -0600

    test: Add docker image and config file checks to serverspec kitchen tests

--- test/integration/default/serverspec/service_spec.rb
+++ test/integration/default/serverspec/service_spec.rb
@@ -5,3 +5,11 @@ set :backend, :exec
 describe service('mesos-dns') do
   it { should be_enabled }
 end
+
+describe docker_image('mesosphere/mesos-dns:latest') do
+  it { should exist }
+end
+
+describe file('/etc/mesos-dns-config.json') do
+  it { should be_file }
+end
